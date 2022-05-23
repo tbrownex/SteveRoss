67309f16da1adba10dce5ab90d48bf8053d42a48
tbrownex <tbrownex@gmail.com>
2022-05-23 20:55:49 +0000

    v2

---
+++ addAddressableTargetToCampaign.py
@@ -0,0 +1,20 @@
+import json
+
+from utils import putRequest
+from config import getBaseURL
+
+def addAddressableTargetToCampaign(orgID, campaignID, addressID):
+    '''
+    Addressable targets are defined at the Org level
+    This will assign one of them to a Campaign
+    '''
+    base = getBaseURL()
+    url = base + "/campaigns/"+str(campaignID)+'/campaign_addresses/change'
+    d = {'add': [
+        {"address_id": addressID,
+         "segment_target_type_id": 2
+        }]
+        }
+    payload = {'campaign_first_party_segments': d}
+    payload = json.dumps(payload)
+    return putRequest(url, payload)
